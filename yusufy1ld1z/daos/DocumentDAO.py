8aacb1bd623d763e32298d2b1d134c65c64e8024
Yusuf Yıldız <117518647+yusufy1ld1z@users.noreply.github.com>
2023-11-27 11:53:20 +0300

    Update DocumentDAO.py

--- yusufy1ld1z/daos/DocumentDAO.py
+++ yusufy1ld1z/daos/DocumentDAO.py
@@ -4,22 +4,22 @@ from db import db
 
 
 class DocumentDAO:
-    @staticmethod
-    def get_all_documents():
-        return Document.query.all()
+    # @staticmethod
+    # def get_all_documents():
+    #     return Document.query.all()
 
-    @staticmethod
-    def get_document_by_documentnode(documentnode):
-        return Document.query.filter_by(documentnode=documentnode).first()
+    # @staticmethod
+    # def get_document_by_documentnode(documentnode):
+    #     return Document.query.filter_by(documentnode=documentnode).first()
 
-    @staticmethod
-    def get_documents_by_status(status):
-        return Document.query.filter_by(status=status).all()
+    # @staticmethod
+    # def get_documents_by_status(status):
+    #   return Document.query.filter_by(status=status).all()
 
-    # def findAll():
-    #     result = db.session.execute(text("SELECT * FROM document.document"))
-    #     column_names = result.keys()
-    #     documents = [
-    #         Document(**dict(zip(column_names, row))) for row in result.fetchall()
-    #     ]
-    #     return documents
+    def findAll():
+        result = db.session.execute(text("SELECT * FROM document.document"))
+        column_names = result.keys()
+        documents = [
+            Document(**dict(zip(column_names, row))) for row in result.fetchall()
+        ]
+        return documents
