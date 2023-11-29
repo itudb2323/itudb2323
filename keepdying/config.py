100bf6994a1572cd5de11ea68d03b08cbdc6781d
Emre Cakiroglu <y.emrecakiroglu@protonmail.com>
2023-11-29 21:37:49 +0300

    chore(product): update config

--- keepdying/config.py
+++ keepdying/config.py
@@ -1,13 +1,15 @@
 class Config:
+    DEBUG = True
+    USE_RELOADER = True
     HOST = '0.0.0.0'
-    PORT = 5000
+    PORT = 5001
     
     SECRET_KEY = "super-secret-key"
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DB_USERNAME = "postgres"
     DB_PASSWORD = "postgres"
-    DB_NAME = "adventureworks"
+    DB_NAME = "Adventureworks"
     SQLALCHEMY_DATABASE_URI = (
         f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@localhost/{DB_NAME}"
     )
