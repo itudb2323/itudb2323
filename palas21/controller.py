ef7e446866240ab2dbea21edc8deec5cbabe2a40
palas21 <palacmpe@gmail.com>
2023-12-21 14:33:06 +0300

    Update.html and some of its basic functions are implemented.

--- palas21/controller.py
+++ palas21/controller.py
@@ -1,13 +1,24 @@
-from flask import Blueprint, render_template
-from model import get_db_data
+from flask import Blueprint, render_template, request
+from model import get_employees, update_employee, add_employee 
 
 index = Blueprint('index', __name__)
 
 @index.route('/')
 def home():
-    data = get_db_data()
+    data = get_employees()
     return render_template('index.html', items=data)
 
 @index.route('/hire')
 def hire():
     return render_template('hire.html')
+
+@index.route('/update/<id>', methods=['GET', 'POST'])
+def update(id):
+    if request.method == 'POST':
+        id = str(request.form.get('id'))
+        print(id.__class__)
+        item = update_employee(id)
+        return render_template('update.html', item=item)
+    else:
+        item = update_employee(id)
+        return render_template('update.html', item=item)
--- palas21/model.py
+++ palas21/model.py
@@ -1,13 +1,20 @@
 import psycopg2
 
-def get_db_data():
+def get_employees():
     conn = psycopg2.connect(database="Adventureworks", user="postgres", password="test", host="127.0.0.1", port="5432")
     cur = conn.cursor()
-    cur.execute("SELECT employee.jobtitle, person.firstname, person.middlename, person.lastname FROM humanresources.employee join person.person on employee.businessentityid = person.businessentityid")
+    cur.execute("SELECT employee.jobtitle, person.firstname, person.middlename, person.lastname, employee.businessentityid FROM humanresources.employee join person.person on employee.businessentityid = person.businessentityid")
     rows = cur.fetchall()
     return rows
 
-def update_db():
+def update_employee(id):
+    conn = psycopg2.connect(database="Adventureworks", user="postgres", password="test", host="127.0.0.1", port="5432")
+    cur = conn.cursor()
+    cur.execute("SELECT employee.jobtitle, person.firstname, person.middlename, person.lastname FROM humanresources.employee join person.person on employee.businessentityid = person.businessentityid where employee.businessentityid = %s", (id,))
+    row = cur.fetchall()
+    return row
+
+def add_employee():
     conn = psycopg2.connect(database="Adventureworks", user="postgres", password="test", host="127.0.0.1", port="5432")
     cur = conn.cursor()
     cur.execute("")
